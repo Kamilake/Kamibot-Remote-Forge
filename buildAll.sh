aafa59c2702c9fc4c96b0186669b51f941d5bc5a
Kamilake <exjang0@gmail.com>
2024-06-13 12:34:06 +0900

    간단 빌드 스크립트 완성!

--- buildAll.sh
+++ buildAll.sh
@@ -2,9 +2,20 @@
 rm -vrf release && \
 mkdir -v release
 
+# # 1.19.4
+# rm -v gradle.properties && \
+# cp -v gradle.properties_1.19.4 gradle.properties && \
+# rm -v build.gradle && \
+# cp -v build.gradle_1.19.4 build.gradle && \
+# rm -vrf build/libs && \
+# ./gradlew build && \
+# mv -v build/libs/*-all.jar release/Kamibot-forge-1.19.4-0.0.1.jar
+
 # 1.20.1
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.1 gradle.properties && \
+rm -v build.gradle && \
+cp -v build.gradle_1.20.1 build.gradle && \
 rm -vrf build/libs && \
 ./gradlew build && \
 mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.1-0.0.1.jar
@@ -12,6 +23,8 @@ mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.1-0.0.1.jar
 # 1.20.2
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.2 gradle.properties && \
+rm -v build.gradle && \
+cp -v build.gradle_1.20.2 build.gradle && \
 rm -vrf build/libs && \
 ./gradlew build && \
 mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.2-0.0.1.jar
@@ -19,6 +32,8 @@ mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.2-0.0.1.jar
 # 1.20.3
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.3 gradle.properties && \
+rm -v build.gradle && \
+cp -v build.gradle_1.20.3 build.gradle && \
 rm -vrf build/libs && \
 ./gradlew build && \
 mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.3-0.0.1.jar
@@ -26,6 +41,8 @@ mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.3-0.0.1.jar
 # 1.20.4
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.4 gradle.properties && \
+rm -v build.gradle && \
+cp -v build.gradle_1.20.4 build.gradle && \
 rm -vrf build/libs && \
 ./gradlew build && \
 mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.4-0.0.1.jar
@@ -33,6 +50,8 @@ mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.4-0.0.1.jar
 # 1.20.6
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.6 gradle.properties && \
+rm -v build.gradle && \
+cp -v build.gradle_1.20.6 build.gradle && \
 rm -vrf build/libs && \
 ./gradlew build && \
 mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.6-0.0.1.jar
@@ -43,4 +62,6 @@ mv -v build/libs/*-all.jar release/Kamibot-forge-1.20.6-0.0.1.jar
 rm -vrf build/libs && \
 rm -v gradle.properties && \
 cp -v gradle.properties_1.20.1 gradle.properties
+rm -v build.gradle && \
+cp -v build.gradle_1.20.1 build.gradle
 
