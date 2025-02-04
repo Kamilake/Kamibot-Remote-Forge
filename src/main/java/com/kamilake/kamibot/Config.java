8a58ab0c62d1e4d16ffc81b814f35cae1491dd60
Kamilake <exjang0@gmail.com>
2025-02-04 15:09:36 +0900

    대청소

--- src/main/java/com/kamilake/kamibot/Config.java
+++ src/main/java/com/kamilake/kamibot/Config.java
@@ -1,18 +1,14 @@
 package com.kamilake.kamibot;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
@@ -33,10 +29,10 @@ public class Config {
       .comment("디버깅을 위한 상세 로깅 활성화 (boolean)")
       .define("enableVerboseLogging", false);
 
-  // a list of strings that are treated as resource locations for items
-  private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-      .comment("A list of items to log on common setup.")
-      .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+  // // a list of strings that are treated as resource locations for items
+  // private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
+  // .comment("A list of items to log on common setup.")
+  // .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
 
 
   static final ForgeConfigSpec SPEC = BUILDER.build();
@@ -47,19 +43,15 @@ public class Config {
   public static boolean enableVerboseLogging;
   public static Set<Item> items;
 
-  private static boolean validateItemName(final Object obj) {
-    return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
-  }
-
   @SubscribeEvent
   static void onLoad(final ModConfigEvent event) {
     kamibotSocketUrl = KAMIBOT_SOCKET_URL.get();
     kamibotRemoteUuid = KAMIBOT_REMOTE_UUID.get();
     enableVerboseLogging = ENABLE_VARBOSE_LOGGING.get();
 
-    // convert the list of strings into a set of items
-    items = ITEM_STRINGS.get().stream()
-        .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
-        .collect(Collectors.toSet());
+    // // convert the list of strings into a set of items
+    // items = ITEM_STRINGS.get().stream()
+    // .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
+    // .collect(Collectors.toSet());
   }
 }
